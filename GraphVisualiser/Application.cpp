02b1b81a4721768bc46113f32981a4d23c949dfe
Venkatesh Kannan <zhac358@live.rhul.ac.uk>
2020-06-16 19:58:31 +0100

    changed opengl versions

--- GraphVisualiser/Application.cpp
+++ GraphVisualiser/Application.cpp
@@ -30,11 +30,11 @@ const float verticesLine[] = {
 
 int main() {
 	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GLFW_VERSION_MAJOR);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GLFW_VERSION_MINOR);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "3D Letter E", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "3D Letter E", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create a window" << std::endl;
 		glfwTerminate();
